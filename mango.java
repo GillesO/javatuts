8eec6dc45c3a3509cc8363408f5c44ffe99cf5bd
GillesO <gillesoloume@gmail.com>
2019-03-25 13:53:11 +0800

    odne with 5: variable

--- mango.java
+++ mango.java
@@ -1,6 +1,12 @@
 class mango{
 	public static void main (String args[]){
-		System.out.println("Hello World");
-	}	
+		double cheese;
+		cheese = 10.00;
 
+
+		System.out.print("need to eat");
+		System.out.print(cheese);
+		System.out.println("cinema");
+		System.out.print("mango");
+	}	
 }
