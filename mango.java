fe6fbdb911fd9c86169e493e718d72c7a55b832a
GillesO <gillesoloume@gmail.com>
2019-03-27 23:22:03 +0800

    done with 13: While loop

--- mango.java
+++ mango.java
@@ -2,22 +2,11 @@
 
 class mango{
 	public static void main (String args[]){
-		int age;
-		age = 3;
+		int counter = 5;
 
-		switch (age){
-		case 1:
-			System.out.println("you can go home");
-			break;
-		case 2:
-			System.out.println("you can warlk");
-			break;
-		case 3:
-			System.out.println("you can grab");
-			break;
-		default:
-			System.out.println("I dont know old are you");
-		break;	
+		while (counter < 10){
+			System.out.println(counter);
+			counter++;
 		}
 	}	
 }
