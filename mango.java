8876b8f88ad3e61edd9612c74f68f7af490f91df
GillesO <gillesoloume@gmail.com>
2019-03-27 12:02:09 +0800

    done with 7: Building a basic calculator

--- mango.java
+++ mango.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 class mango{
 	public static void main (String args[]){
 		Scanner duck = new Scanner(System.in);
-		System.out.println(duck.nextLine());
+		double dnum, anum, answer;
+		System.out.println("Enter different num: ");
+		dnum = duck.nextDouble();
+		System.out.println("Enter another num: ");
+		anum = duck.nextDouble();
+		answer = dnum + anum;
+		System.out.println(answer);
 
 	}	
 }
