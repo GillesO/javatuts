78da3b44a598aab68156f88ccc64e778a6aa2ed5
GillesO <gillesoloume@gmail.com>
2019-03-27 13:07:27 +0800

    done with 9: if Statement

--- mango.java
+++ mango.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 class mango{
 	public static void main (String args[]){
-		Scanner duck = new Scanner(System.in);
+		int test = 22;
 
-		int egg = 8;
-		int veggie = 22;
-
-		egg *=5;
-
-		System.out.println (egg);
+		if  (test < 10) {
+			System.out.println("yes");
+		}else{
+			System.out.println("this is else");
+		}
 	}	
 }
